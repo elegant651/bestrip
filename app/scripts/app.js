b3968c0d8a6759f8b611675fb8b00566d4c5d5da
elegantuniv <khpark@knowre.com>
2015-11-20 12:48:38 +0900

    ignore

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,46 +14,46 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   var app = document.querySelector('#app');
 
 
-  // var webComponentsSupported = (
-  //     'registerElement' in document &&
-  //     'import' in document.createElement('link') &&
-  //     'content' in document.createElement('template'));
+  var webComponentsSupported = (
+      'registerElement' in document &&
+      'import' in document.createElement('link') &&
+      'content' in document.createElement('template'));
 
-  // if(!webComponentsSupported) {
-  //   var script = document.createElement('script');
-  //   script.async = true;
-  //   script.src = 'bower_components/webcomponentsjs/webcomponents-lite.min.js';
-  //   script.onload = finishLazyLoadingImports;
-  //   document.head.appendChild(script);
-  // } else {
-  //   finishLazyLoadingImports();
-  //   var loadContainer = document.getElementById('splash');
-  //   loadContainer.parentNode.removeChild(loadContainer);
-  // }
+  if(!webComponentsSupported) {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'bower_components/webcomponentsjs/webcomponents-lite.min.js';
+    script.onload = finishLazyLoadingImports;
+    document.head.appendChild(script);
+  } else {
+    finishLazyLoadingImports();
+    var loadContainer = document.getElementById('splash');
+    loadContainer.parentNode.removeChild(loadContainer);
+  }
 
-  // function finishLazyLoadingImports() {
-  //   // Use native Shadow DOM if it's available in the browser.
-  //   window.Polymer = window.Polymer || {dom: 'shadow'};
+  function finishLazyLoadingImports() {
+    // Use native Shadow DOM if it's available in the browser.
+    window.Polymer = window.Polymer || {dom: 'shadow'};
 
-  //   var onImportLoaded = function() {
-  //     var loadContainer = document.getElementById('splash');
-  //     loadContainer.addEventListener('transitionend', function(e){
-  //       loadContainer.parentNode.removeChild(loadContainer); // IE 10 doesn't support el.remove()
-  //       console.log("loaded");
-  //     });      
+    var onImportLoaded = function() {
+      var loadContainer = document.getElementById('splash');
+      loadContainer.addEventListener('transitionend', function(e){
+        loadContainer.parentNode.removeChild(loadContainer); // IE 10 doesn't support el.remove()
+        console.log("loaded");
+      });      
 
-  //     document.body.classList.remove('loading');
-  //   };
+      document.body.classList.remove('loading');
+    };
 
-  //   // crbug.com/504944 - readyState never goes to complete until Chrome 46.
-  //   // crbug.com/505279 - Resource Timing API is not available until Chrome 46.
-  //   var link = document.querySelector('#bundle');
-  //   if (link.import && link.import.readyState === 'complete') {
-  //     onImportLoaded();
-  //   } else {
-  //     link.addEventListener('load', onImportLoaded);
-  //   }
-  // }
+    // crbug.com/504944 - readyState never goes to complete until Chrome 46.
+    // crbug.com/505279 - Resource Timing API is not available until Chrome 46.
+    var link = document.querySelector('#bundle');
+    if (link.import && link.import.readyState === 'complete') {
+      onImportLoaded();
+    } else {
+      link.addEventListener('load', onImportLoaded);
+    }
+  }
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
